3deb4be28fc0fda1474af1e95177628391306630
coldsewoo <coldsewoo@gmail.com>
2019-08-14 12:31:08 +0900

    added avatar url in message schema

--- db/message.js
+++ db/message.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const messageSchema = new mongoose.Schema({
   username : {type: String, default: "Anonymous"},
-  message: {type: String, required: true}
+  message: {type: String, required: true},
+  avatar: {type: String, default : ""}
 })
 
 module.exports = mongoose.model("messages", messageSchema)
